4ee123dc17d080231f46a809aaa73e0b1c94bf9b
bxphxmxth <fushs@hotmail.fr>
2021-05-04 20:07:42 +0200

    trvartemp

---
+++ src/js/main.js
@@ -0,0 +1,83 @@
+// # Variables temporaires
+//  let a = 1; // -> 2
+//  let b = 2; // -> 3
+//  let c = 3; // -> 1
+//     let temp = 1
+// a = b; //2
+// b = c; //3
+// c = temp; //1
+
+// console.log(a,b,c);
+
+
+
+//     - ## Afficher le changement des variables
+
+// - # Exo 1
+
+//  Afficher 4, 3, 2, 1
+//  let a = 1; // -> 4
+//  let b = 2;  // -> 3
+//  let c = 3;  // -> 2
+//  let d = 4;  // -> 1
+
+//  let temp;
+
+//  temp = a; // 1
+//  a = d; //4
+//  d = temp; //1
+//  temp = b; //2
+//  b = c; //3 
+//  c = temp //2 
+
+
+//     - ## Afficher le changement des variables
+
+// console.log(a,b,c,d);
+
+// - #  Exo 2
+// let a = 1; // -> 2
+// let b = 2; // -> 3
+// let c = 3; // -> 4
+// let d = 4; // -> 1
+
+// temp = a; // 1
+// a = b; // 2
+// b = c; //3
+// c = d; // 4 
+// d = temp; // 1 
+
+//     - ## Afficher le changement des variables
+
+// console.log(a,b,c,d,);
+
+// - # Exo 3
+//     - ## Afficher 4, 3, 2, 1 avec une variable temporaire
+// let tableau = [1, 2, 3, 4, "temp"];
+
+// tableau[4] = tableau[0]; //1
+// tableau[0] = tableau[3];
+// tableau[3] = tableau[]
+
+
+
+//     - # Exo 4
+let a = 1 //-> 4
+let b = 2 //-> 5
+let c = 3 //-> 1
+let d = 4 //-> 2
+let e = 5 //-> 6
+let f = 6 //-> 3
+
+temp = a; // 1 
+a = d; //4
+d = b; //2
+b = e; //5
+e = f; //6
+f = c;
+c = temp;
+
+
+//     - ## Afficher le changement des variables
+
+console.log(a,b,c,d,e,f);
